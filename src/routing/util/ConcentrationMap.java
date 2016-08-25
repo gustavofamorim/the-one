0ce90afc05726aee3d7b0a42598400e6c931d544
gustavofamorim <gustavofreitasamorim@gmail.com>
2016-08-25 00:22:23 -0300

    Changed the Tuple<> to Coord to represent coordinates.

--- src/routing/util/ConcentrationMap.java
+++ src/routing/util/ConcentrationMap.java
@@ -1,17 +1,14 @@
-package core;
+package routing.util;
 
-import util.Tuple;
+import core.Coord;
+import core.DTNHost;
+import core.Settings;
 
-import java.util.HashMap;
+import java.util.TreeMap;
 
 /** The concentration gradient map of the simulation map.
  */
 public class ConcentrationMap implements Cloneable{
-
-    /** Delete delivered messages -setting id ({@value}). Boolean valued.
-     * If set to true and final recipient of a message rejects it because it
-     * already has it, the message is deleted from buffer. Default=false. */
-    public static final String CONCENTRATION_MAP_PREFIX_S = "ConcentrationMapping.";
 
     /** The granularity -setting id ({@value}). Double valued.
      * Defines the side of all square regions mapped.
@@ -22,15 +19,16 @@ public class ConcentrationMap implements Cloneable{
     private static double granularity;
 
     /** Concentration gradient map.
-     * The key {@code Tuple<Double, Double>} indicate the left botton point of the
+     * The key {@code Coord} indicate the left botton point of the
      * region.
      * The value {@code Double} indicate the registred number of contacts in the region
      * identified by the key.
      */
-    private HashMap<Tuple<Double, Double>, Long> map;
+    private TreeMap<Coord, Long> map;
 
-    public ConcentrationMap(Settings s){
-
+    public ConcentrationMap(DTNHost host, Settings s){
+        this.granularity = s.getDouble(GRANULARITY_MAP_S);
+        this.map = new TreeMap<>();
     }
 
     /**
@@ -38,10 +36,10 @@ public class ConcentrationMap implements Cloneable{
      * @param mapLocation The actual location of the node.
      * @return The tuble of the region wich includes de coordinate.
      */
-    private Tuple<Double, Double> convertMapLocationToRegionKey(Coord mapLocation){
+    private Coord convertMapLocationToRegionKey(Coord mapLocation){
         Double x = Math.floor(mapLocation.getX() / this.granularity);
         Double y = Math.floor(mapLocation.getY() / this.granularity);
-        return (new Tuple<Double, Double>(x,y));
+        return (new Coord(x,y));
     }
 
     /**
@@ -49,33 +47,26 @@ public class ConcentrationMap implements Cloneable{
      * @param coord The region coordinate.
      * @return The concentration of a region.
      */
-    public double getConcentration(Tuple<Double, Double> coord){
-        if (this.map.containsKey(coord)){
-            return (this.map.get(coord) / (this.granularity * this.granularity));
+    public double getConcentration(Coord coord){
+        Coord regionBase = this.convertMapLocationToRegionKey(coord);
+        if (this.map.containsKey(regionBase)){
+            return (this.map.get(regionBase) / (this.granularity * this.granularity));
         }
         //Region not mapped yet
         return (0);
     }
 
     /**
-     * Returns the concentration of a region were the node is.
-     * @param nodeLocation The actual location of the node.
-     * @return The concentration of a region.
-     */
-    public double getConcentration(Coord nodeLocation){
-        return (this.getConcentration(this.convertMapLocationToRegionKey(nodeLocation)));
-    }
-
-    /**
      * Record a contact event on the region were the node is.
      * @param nodeLocation The actual location of the node.
      */
-    public void recordContact(Tuple<Double, Double> nodeLocation){
-        if(!this.map.containsKey(nodeLocation)){
-            this.map.put(nodeLocation, (long) 1);
+    public void recordContact(Coord nodeLocation){
+        Coord regionOfNode = this.convertMapLocationToRegionKey(nodeLocation);
+        if(!this.map.containsKey(regionOfNode)){
+            this.map.put(regionOfNode, new Long(1));
         }
         else{
-            this.map.replace(nodeLocation, this.map.get(nodeLocation) + 1);
+            this.map.replace(regionOfNode, new Long(this.map.get(regionOfNode) + 1));
         }
     }
 
@@ -84,7 +75,7 @@ public class ConcentrationMap implements Cloneable{
      * @param anotherMap The map received from another node.
      */
     public void mergeConcentrationMap(ConcentrationMap anotherMap){
-        for(Tuple<Double, Double> key : anotherMap.map.keySet()){
+        for(Coord key : anotherMap.map.keySet()){
             //If this map contais the region, sum the numbers contacts in the region
             if(this.map.containsKey(key)){
                 this.map.replace(key, this.map.get(key) + anotherMap.map.get(key));
@@ -94,6 +85,21 @@ public class ConcentrationMap implements Cloneable{
             }
         }
     }
+
+    /** Returns the routing info of this concentration map.
+     *  @return The routing info of this concentration map.
+     */
+    public RoutingInfo makeRountingInfo(){
+        RoutingInfo concentrationMap = new RoutingInfo("Concentration Map:");
+
+        for(Coord coord : this.map.keySet()){
+            RoutingInfo info = new RoutingInfo("Region (" + coord.getX() + ", " + coord.getY() + "): " + this.map.get(coord));
+            concentrationMap.addMoreInfo(info);
+        }
+
+        return (concentrationMap);
+    }
+
     /**
      * Clone this concentration map.
      * @return A clone of this map.
