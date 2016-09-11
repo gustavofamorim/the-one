2aeebcf237a066129468cf3b2b2a585a4c2dd78d
gustavofamorim <gustavofreitasamorim@gmail.com>
2016-09-11 19:17:58 -0300

    FIX: Import fix.

--- src/report/EnergyLevelReport.java
+++ src/report/EnergyLevelReport.java
@@ -8,13 +8,14 @@ package report;
 import core.DTNHost;
 import core.SimError;
 import core.UpdateListener;
+import routing.util.EnergyModel.EnergyModel;
 
 /**
  * Node energy level report. Reports the energy level of all 
  * (or only some, see {@link #REPORTED_NODES}) nodes every 
  * configurable-amount-of seconds (see {@link #GRANULARITY}).
  * Works only if all nodes use energy model; see 
- * {@link routing.util.EnergyModel}.
+ * {@link EnergyModel}.
  */
 public class EnergyLevelReport extends SnapshotReport 
 	implements UpdateListener {
@@ -22,7 +23,7 @@ public class EnergyLevelReport extends SnapshotReport
 	@Override
 	protected void writeSnapshot(DTNHost h) {
 		Double value = (Double)h.getComBus().
-				getProperty(routing.util.EnergyModel.ENERGY_VALUE_ID);
+				getProperty(EnergyModel.ENERGY_VALUE_ID);
 			if (value == null) {
 				throw new SimError("Host " + h +
 						" is not using energy model");
